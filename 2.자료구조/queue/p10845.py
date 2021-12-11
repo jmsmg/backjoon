90af8cc46b8b10a9922e263695ec9ea1718d9638
jmsmg <jmsmg1@me.com>
2021-12-11 10:07:51 +0900

    queue 구현문제

---
+++ 2.자료구조/queue/p10845.py
@@ -0,0 +1,50 @@
+import sys
+def push(x):
+    queue.append(x)
+
+def size():
+    print(len(queue))
+
+def pop():
+    if (len(queue) == 0):
+        print(-1)
+    else:
+        print(queue.pop(0))
+        
+def empty():
+    if (len(queue) == 0):
+        print(1)
+    else:
+        print(0)
+
+def front():
+    if (len(queue) == 0):
+        print(-1)
+    else:
+        print(queue[0])
+
+def back():
+    if (len(queue) == 0):
+        print(-1)
+    else:
+        print(queue[-1])
+
+queue = []
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    a = sys.stdin.readline().split()
+    
+    if (a[0] == 'push'):
+        push(a[1])
+    elif (a[0] == 'pop'):
+        pop()
+    elif (a[0] == 'size'):
+        size()
+    elif (a[0] == 'empty'):
+        empty()
+    elif (a[0] == 'front'):
+        front()
+    elif (a[0] == 'back'):
+        back()
