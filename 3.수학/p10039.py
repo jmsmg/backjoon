8727b222e8619f6c8becb63e0d80df0e77cdcfab
jmsmg <jmsmg1@me.com>
2022-01-14 12:04:34 +0000

    average

--- 3.수학/p10039.py
+++ 3.수학/p10039.py
@@ -1,3 +1,13 @@
 import sys
+a = [0] * 5
+answer = 0
+for i in range(5):
+   a[i] = int(sys.stdin.readline().rstrip())
 
-list(map(int, sys.stdin.readline()))
+for i in range(5):
+    if a[i] < 40:
+        answer += 40
+    else:
+        answer += a[i]
+
+print(answer//5)
